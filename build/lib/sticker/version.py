d78c42879a01c51dc0f8c6e0c1443253687884cd
xftsfyi <fts@lowfla.me>
2025-05-09 13:15:34 -0600

    add neocat and neofox

--- build/lib/sticker/version.py
+++ build/lib/sticker/version.py
@@ -1,6 +1,6 @@
 # Generated in setup.py
 
 git_tag = None
-git_revision = '4b96d236'
-version = '0.1.0+dev.4b96d236'
-linkified_version = '0.1.0+dev.[4b96d236](https://github.com/maunium/stickerpicker/commit/4b96d236212b1212976f4c3c60479e7aaed866cb)'
+git_revision = 'ab2daead'
+version = '0.1.0+dev.ab2daead'
+linkified_version = '0.1.0+dev.[ab2daead](https://github.com/maunium/stickerpicker/commit/ab2daeadb6f364c54615c617ee1b03a97437cc79)'
