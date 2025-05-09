fd643cf75cc0a005035253288a8fbbf48b8fba93
xftsfyi <fts@lowfla.me>
2025-05-09 13:37:45 -0600

    fix (hopefully?)

--- build/lib/sticker/version.py
+++ build/lib/sticker/version.py
@@ -1,6 +1,6 @@
 # Generated in setup.py
 
 git_tag = None
-git_revision = 'ab2daead'
-version = '0.1.0+dev.ab2daead'
-linkified_version = '0.1.0+dev.[ab2daead](https://github.com/maunium/stickerpicker/commit/ab2daeadb6f364c54615c617ee1b03a97437cc79)'
+git_revision = 'd78c4287'
+version = '0.1.0+dev.d78c4287'
+linkified_version = '0.1.0+dev.[d78c4287](https://github.com/maunium/stickerpicker/commit/d78c42879a01c51dc0f8c6e0c1443253687884cd)'
